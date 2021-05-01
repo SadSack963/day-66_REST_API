6466c8d14a94bc3de5425cc26076fadf5dd3f62c
john <john@jpatmore.plus.com>
2021-05-01 22:36:31 +0100

    Get all cafes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 
 # Cafe TABLE Configuration
 class Cafe(db.Model):
-    # __tablename__ = "Cafes"
+    __tablename__ = "Cafes"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
